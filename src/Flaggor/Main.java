0dbae74a1d34d2f0f5f42853bb50d7e67998ca79
theo0165 <theo01sandell@gmail.com>
2019-09-25 11:20:51 +0200

    Skapade main.java i Flaggor

---
+++ src/Flaggor/Main.java
@@ -0,0 +1,20 @@
+package Flaggor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    /**
+     * Launch program
+     * @param args
+     */
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Flaggor");
+        
+    }
+}
