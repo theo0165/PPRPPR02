a26106d0e2d5236a8dd13cfd43de16c247a02ff1
theo0165 <theo01sandell@gmail.com>
2019-09-26 14:37:03 +0200

    La till italien

--- src/Flaggor/Flags/Italy.java
+++ src/Flaggor/Flags/Italy.java
@@ -1,8 +1,46 @@
 package Flaggor.Flags;
 
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
 public class Italy extends Flag {
     @Override
-    public String getName() {
-        return "Sweden";
+    public String getName() { return "Italy"; }
+
+    @Override
+    public Node renderFlag() {
+        StackPane flagContainer = new StackPane();
+        flagContainer.setAlignment(Pos.CENTER);
+
+        Rectangle green = new Rectangle();
+        green.setWidth(133);
+        green.setHeight(200);
+        green.setFill(Color.rgb(0, 122, 51));
+        green.setTranslateX(0);
+        green.setTranslateY(0);
+
+        Rectangle white = new Rectangle();
+        white.setWidth(133);
+        white.setHeight(200);
+        white.setFill(Color.WHITE);
+        white.setTranslateX(133);
+        white.setTranslateY(0);
+
+        Rectangle red = new Rectangle();
+        red.setWidth(133);
+        red.setHeight(200);
+        red.setFill(Color.rgb(203, 51, 59));
+        red.setTranslateX(266);
+        red.setTranslateY(0);
+
+        flagContainer.getChildren().add(green);
+        flagContainer.getChildren().add(white);
+        flagContainer.getChildren().add(red);
+
+        return flagContainer;
     }
 }
