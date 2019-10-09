893b04fbc5f4a950cc9207e4789f44ace2216c3f
theo0165 <theo01sandell@gmail.com>
2019-10-09 12:06:38 +0200

    Created regular expression that only allows math

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,11 +1,16 @@
 package Calculator;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.regex.Pattern;
 
 public class Main extends Application {
     private static BorderPane mainLayout = new BorderPane();
@@ -26,6 +31,17 @@ public class Main extends Application {
     private static VBox getIOField(){
         VBox ioBox = new VBox();
         TextField ioField = new TextField();
+        ioField.setPrefHeight(50);
+        ioField.setAlignment(Pos.CENTER_RIGHT);
+
+        ioField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(Pattern.matches(newValue, "^(\\d|\\+|\\-|\\*|\\/|\\(|\\)|\\^|\\%|sqrt)*$")){
+                    ioField.setText(newValue.replaceAll("[^(\\d|\\+|\\-|\\*|\\/|\\(|\\)|\\^|\\%|sqrt)]", ""));
+                }
+            }
+        });
 
         ioBox.getChildren().add(ioField);
 
